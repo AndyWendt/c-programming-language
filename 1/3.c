14497a8b24ab5e8f456719a2541f28837fd2802f
Andy Wendt <andy@awendt.com>
2017-11-02 13:51:23 -0600

    Rename

---
+++ 1/3.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+main()
+{
+    // Exercise is to see what happens when you don't use a \t or \n and instead use a random letter
+    // it prints the "y"
+    printf("Hellow, world\ty");
+}
