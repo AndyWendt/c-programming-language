2f8ed4f1fe715a0c994a3617082ca79e0798352f
Andy Wendt <andy@awendt.com>
2017-11-02 14:02:46 -0600

    Add 1/3.c with column headers

--- 1/3.c
+++ 1/3.c
@@ -1,8 +1,25 @@
 #include <stdio.h>
 
+/* ### Exercise 1.3 ###
+ * Modify the temperature conversion program to print a heading above the table
+ */
+
 main()
 {
-    // Exercise is to see what happens when you don't use a \t or \n and instead use a random letter
-    // it prints the "y"
-    printf("Hellow, world\ty");
+
+    float fahr, celcius;
+    int lower, upper, step;
+
+    lower = 0;
+    upper = 300;
+    step = 20;
+
+    fahr = lower;
+
+    printf("%5s\t%6s\n", "Fahr", "Celc");
+    while (fahr <= upper) {
+        celcius = (5.0/9.0) * (fahr - 32.0);
+        printf("%5.0f\t%6.1f\n", fahr, celcius);
+        fahr = fahr + step;
+    }
 }
