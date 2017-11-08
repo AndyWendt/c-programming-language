8bde46d6c9acebd20773316bc1cbe88ad995eaa8
Andy Wendt <andy@awendt.com>
2017-11-08 16:57:25 -0700

    Update exercise 4

--- 1/4.c
+++ 1/4.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
-/* ### Exercise 1.3 ###
- * Modify the temperature conversion program to print a heading above the table
+/* ### Exercise 1.4 ###
+ * Write a program to print the corresponding Celsius to Fahrenheit table
  */
 
 main()
