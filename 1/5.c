052d0f03ec32c429c6f8fb76de5f446ec182af4e
Andy Wendt <andy@awendt.com>
2017-11-09 08:22:50 -0700

    Update exercise 1.5 with #defines

--- 1/5.c
+++ 1/5.c
@@ -3,6 +3,9 @@
 /* ### Exercise 1.5 ###
  * Modify the temperature conversion program to print the table in reverse order, that is, from 300 degrees to 0.
  */
+#define LOWER 0
+#define UPPER 300
+#define STEP 20
 
 main()
 {
@@ -10,7 +13,7 @@ main()
 
     printf("%5s\t%6s\n", "Fahr", "Celc");
 
-    for (fahr = 300; fahr >= 0; fahr = (fahr - 20)) {
+    for (fahr = UPPER; fahr >= LOWER; fahr = (fahr - STEP)) {
         printf("%d\t%6.1f\n", fahr, (5.0/9.0) * (fahr - 32));
     }
 }
